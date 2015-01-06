157aea0edba1f34149ba3943e8e22ad1c6101d0f
Administrator <Administrator@HEO>
2015-01-06 23:48:39 +0900

    sqlmap 테스트 복원

--- src/test/java/com/lin/clould/common/database/ibatis/SqlMapClientManagerTest.java
+++ src/test/java/com/lin/clould/common/database/ibatis/SqlMapClientManagerTest.java
@@ -1,6 +1,5 @@
 package com.lin.clould.common.database.ibatis;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import com.ibatis.sqlmap.client.SqlMapClient;
@@ -10,13 +9,11 @@ public class SqlMapClientManagerTest {
 	
 	private SqlMapClient sqlMap;
 
-	@Ignore
 	@Test
 	public void nothing(){
 		
 	}
 	
-	@Ignore
 	@Test
 	public void canCreateObject(){
 		sqlMap = SqlMapClientManager.getSqlMapClient();
